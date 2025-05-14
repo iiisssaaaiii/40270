09a32d694a359d07faaf7b23a21c1df784b52bf7
Ashborn <isaigalvan12309@gmail.com>
2025-05-14 15:15:43 -0600

    09-10 modulos

--- html00/formulario.js
+++ html00/formulario.js
@@ -6,22 +6,27 @@ function muestra(){
 
     console.log(user.value)
     console.log(password.value)
-
 }
 
-//Ejemplo de funcion de callback
-//Cuando la funcion es de callback no lleva parentesis
-
+//Ejemplo de función de callback
 const boton = document.getElementById("boton")
-/* boton.addEventListener("click", muestra) */
+// boton.addEventListener("click", muestra)
 
-// Ejemplo de fucion anonima de tipo arrow
-boton.addEventListener("click", 
-    ()=>{
+//Ejemplo de función anónima de tipo arrow
+boton.addEventListener("click",
+    () => {
         let user = document.getElementById("user")
         let password = document.getElementById("password")
-
+    
         console.log(user.value)
         console.log(password.value)
+
+        user.setAttribute("name", "q")
+        password.setAttribute("name", "c")
+
+        let formulario = document.getElementsByTagName("form")
+        formulario[0].setAttribute("action","https://www.google.com/search")
+        console.log(formulario[0])
+        formulario[0].submit()
     }
 )
---
+++ html00/tarea/tarea.md
@@ -0,0 +1,21 @@
+# Refactorización de codigo
+
+## Objetivo
+
+Reescribir aquellos ejercicios de html que utilizan javascript de tal manera que se vuelvan a hacer pero su codigo javaScript será reescrito como modulos.
+Estilos modulos javaScript tipo pagina 10.
+
+Escribir un breve reporte, con capturas de pantalla, donde se vea la diferencia del tiempo de carga de cada ejercicio sin refactorizar y refactorizado.
+
+### Lista de Ejercicios
+ - pagina01
+ - pagina02
+ - pagina05
+ - pagina06
+ - pagina07
+
+ ## Envio
+
+ Finalizado el ejercicio se debera subir a github de la forma siguiente
+ git add tarea/tarea.md
+ git commit -m""
